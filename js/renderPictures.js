f84f1a31a6b0bd42420bfdd0789a209bdac92bd5
karina.mazlowa@yandex.ru <karina.mazlowa@yandex.ru>
2025-02-17 18:35:53 +0300

    Добавление отрисовки модального окна - ДЗ8

--- js/renderPictures.js
+++ js/renderPictures.js
@@ -1,16 +1,22 @@
+import { openModal } from "./openModal";
+
 const picturesContainer = document.querySelector('.pictures');
 // const pictureList = pictureContainer.children;
 
 const pictureTemplate = document.querySelector('#picture').content.querySelector('.picture');
 
-const picturesListFragment = document.createDocumentFragment();
+let localData;
 
 export const renderPictures = (picturesData) => {
+  localData = [...picturesData];
+  const picturesListFragment = document.createDocumentFragment();
+
   picturesData.forEach((picture) => {
     const pictureElement = pictureTemplate.cloneNode(true);
     const pictureImg = pictureElement.querySelector('.picture__img');
     pictureImg.src = picture.url;
     pictureImg.alt = picture.description;
+    pictureElement.dataset.pictureId = picture.id;
     pictureElement.querySelector('.picture__comments').textContent = picture.comments.length;
     pictureElement.querySelector('.picture__likes').textContent = picture.likes;
     picturesListFragment.appendChild(pictureElement);
@@ -18,3 +24,12 @@ export const renderPictures = (picturesData) => {
 
   picturesContainer.appendChild(picturesListFragment);
 };
+
+picturesContainer.addEventListener('click', (evt) => {
+  const pictureElement = evt.target.closest('.picture');
+  if(pictureElement) {
+    const id = Number(pictureElement.dataset.pictureId);
+    const pictureData = localData.find((item) => item.id === id);
+    openModal(pictureData);
+  }
+});
